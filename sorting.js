621b0a529aa80a579ebf140786dc8e8e96a1a891
Sanjeev Kumar <sanjeev110592@gmail.com>
2023-07-16 13:53:16 +0530

    Implemented selection sort algorithm

--- sorting.js
+++ sorting.js
@@ -51,4 +51,37 @@ function insertionSort(array) {
 
 let insertionSortResult = insertionSort([6, 3, 1, 9, 8, 10]);
 
-console.log(insertionSortResult);
+// console.log(insertionSortResult);
+
+
+/* ===== Selection sort ===== */
+
+function selectionSort(array) {
+
+    for (let i = 0; i < array.length - 1; i++) {
+
+        let minimumValueIndex = i;
+        let minimumValue = array[i]
+
+        for (let j = i + 1; j < array.length; j++) {
+
+            if (array[j] < minimumValue) {
+
+                minimumValueIndex = j;
+                minimumValue = array[j];
+
+            }
+
+        }
+
+        [array[minimumValueIndex], array[i]] = [array[i], array[minimumValueIndex]];
+
+    }
+
+    return array;
+
+}
+
+let selectionSortResult = selectionSort([6, 3, 1, 9, 8, 10]);
+
+console.log(selectionSortResult);
